5e518db39f5c9a0da6e847c46d3cbf90812842f1
Saurabh <saurabh6790@gmail.com>
2023-05-06 08:17:07 +0530

    feat: 1st assignment

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import bottle # https://bottlepy.org/docs/dev/tutorial_app.html
 import sqlite3
+import json
 
 db = sqlite3.connect(':memory:')
 db.row_factory = sqlite3.Row
@@ -16,6 +17,38 @@ db.executescript('''
 def index():
     bottle.response.content_type = 'text/plain'
     return "Inspire Candidate Exercise"
+
+@bottle.get('/wombats')
+def get_wombats():
+    bottle.response.content_type = 'application/json'
+    cur = db.execute('SELECT * FROM wombat')
+    return { 'wombats': [dict(row) for row in cur.fetchall()] }
+
+@bottle.post('/wombats')
+def post_wombats():
+    bottle.response.content_type = 'application/json'
+
+    name = bottle.request.forms.get('name')
+    if name is None:
+        return bottle.HTTPResponse(status=400, body="Missing parameter: name")   
+
+    dob = bottle.request.forms.get('dob')
+    if dob is None:
+        return bottle.HTTPResponse(status=400, body= "Missing parameter: dob")   
+
+    cur = db.execute('INSERT INTO wombat(name, dob) VALUES(?,?)', (name, dob))
+    return { 'id': cur.lastrowid, 'name': name, 'dob': dob }
+
+@bottle.error(404)
+def error404(error):
+    bottle.response.content_type = 'text/plain'
+    return "Not Found"
+
+@bottle.error(405)
+def error405(error):
+    bottle.response.content_type = 'text/plain'
+    return "Method Not Allowed"
+
 
 if __name__ == '__main__':
     import sys
